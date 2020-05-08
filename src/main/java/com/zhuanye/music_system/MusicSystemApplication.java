2af4aca9cec1e65ea96bce2cf043567e0a3b39fe
CZM <2723081630@qq.com>
2020-05-08 21:18:52 +0800

    添加下载上传歌曲功能

--- src/main/java/com/zhuanye/music_system/MusicSystemApplication.java
+++ src/main/java/com/zhuanye/music_system/MusicSystemApplication.java
@@ -2,9 +2,12 @@ package com.zhuanye.music_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MusicSystemApplication {
+@ServletComponentScan
+public class MusicSystemApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicSystemApplication.class, args);
